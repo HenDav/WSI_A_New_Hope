Bug fixed: - AUC computation for train dataset during training

nets.py
@@ -15,7 +15,6 @@ class Flatten(nn.Module):
 
 class ResNet50_GatedAttention(nn.Module):
     def __init__(self):
-    #def __init__(self, tile_size: int = 256):
         super(ResNet50_GatedAttention, self).__init__()
         #self.tile_size = tile_size
         self.M = 500
@@ -23,8 +22,8 @@ class ResNet50_GatedAttention(nn.Module):
         self.K = 1    # in the paper referred a 1.
 
         self.infer = False
-        self.get_features = False
-        self.get_classification = False
+        self.part_1 = False
+        self.part_2 = False
 
         self._feature_extractor_ResNet50_part_1 = models.resnet50()
 
@@ -55,16 +54,16 @@ class ResNet50_GatedAttention(nn.Module):
             nn.Sigmoid()
         )
 
-    def forward(self, x):
-        x = x.squeeze(0)
-        # In case we are training the model we'll use bags that contains only part of the tiles.
+    def forward(self, x, H = None, A = None):
         if not self.infer:
+            x = x.squeeze(0)
+            # In case we are training the model we'll use bags that contains only part of the tiles.
             H = self.feature_extractor(x)
 
             """
-        H = H.view(-1, 50 * 4 * 4) 
-        H = self.feature_extractor_part2(H)  # NxL
-        """
+            H = H.view(-1, 50 * 4 * 4) 
+            H = self.feature_extractor_part2(H)  # NxL
+            """
 
             A_V = self.attention_V(H)  # NxL
             A_U = self.attention_U(H)  # NxL
@@ -85,17 +84,17 @@ class ResNet50_GatedAttention(nn.Module):
 
         # In case we want an inference of a whole slide we need ALL the tiles from that slide:
         else:
-            if not self.get_features ^ self.get_classification:
-                raise Exception('Inference Mode should include feature extraction OR classification')
-            if self.get_features:
+            if not self.part_1 ^ self.part_2:
+                raise Exception('Inference Mode should include feature extraction (part 1) OR classification (part 2)')
+            if self.part_1:
                 H = self.feature_extractor(x)
-                return H
-
-            elif self.get_classification:
                 A_V = self.attention_V(H)  # NxL
                 A_U = self.attention_U(H)  # NxL
                 A = self.attention_weights(A_V * A_U)  # element wise multiplication # NxK
                 A = torch.transpose(A, 1, 0)  # KxN
+                return H, A
+
+            elif self.part_2:
                 A = F.softmax(A, dim=1)  # softmax over N
 
                 M = torch.mm(A, H)  # KxM

train_mil.py
@@ -117,7 +117,7 @@ def train(model: nn.Module, dloader_train: DataLoader, dloader_test: DataLoader,
             elif target == 0:
                 total_neg_train += 1
                 true_labels_train[batch_idx] = 0
-                if target == 0:
+                if label == 0:
                     true_neg_train += 1
 
             scores_train[batch_idx] = prob.cpu().detach().numpy()[0][0]
@@ -165,12 +165,7 @@ def train(model: nn.Module, dloader_train: DataLoader, dloader_test: DataLoader,
         all_writer.add_scalar('Train/Roc-Auc', roc_auc_train, e)
         all_writer.add_scalar('Train/Loss Per Epoch', train_loss, e)
         all_writer.add_scalar('Train/Accuracy', train_acc, e)
-        """
-        train_writer.add_scalar('Balanced Accuracy', balanced_acc_train, e)
-        train_writer.add_scalar('Roc-Auc', roc_auc_train, e)
-        train_writer.add_scalar('Loss Per Epoch', train_loss, e)
-        train_writer.add_scalar('Accuracy', train_acc, e)
-        """
+
         print('Finished Epoch: {}, Loss: {:.2f}, Loss Delta: {:.3f}, Train Accuracy: {:.2f}% ({} / {}), Time: {:.0f} m'
               .format(e,
                       train_loss,
@@ -189,8 +184,8 @@ def train(model: nn.Module, dloader_train: DataLoader, dloader_test: DataLoader,
             best_model = model
 
         if e % 5 == 0:
-            # acc_test, bacc_test = check_accuracy(model, dloader_test, test_writer, all_writer, DEVICE, e)
-            acc_test, bacc_test = check_accuracy(model, dloader_test, all_writer, DEVICE, e)
+            acc_test, bacc_test = check_accuracy(model, dloader_test, all_writer, DEVICE, e, eval_mode=True)
+            acc_test, bacc_test = check_accuracy(model, dloader_test, all_writer, DEVICE, e, eval_mode=False)
         else:
             acc_test, bacc_test = None, None
 
@@ -242,23 +237,18 @@ def train(model: nn.Module, dloader_train: DataLoader, dloader_test: DataLoader,
     return best_model, best_train_acc, best_train_loss
 
 
-def check_accuracy(model: nn.Module, data_loader: DataLoader, writer_all, DEVICE, epoch: int):
-#def check_accuracy(model: nn.Module, data_loader: DataLoader, writer_test, writer_all, DEVICE, epoch: int):
-#def check_accuracy(model: nn.Module, data_loader: DataLoader, writer, DEVICE, epoch:int, train_string: str ='', train: bool = False):
-#def check_accuracy(data_loader: DataLoader, epoch:int, train: bool = False):
-    """
-       if loader.dataset.train:
-        print('Checking accuracy on validation set')
-    else:
-        print('Checking accuracy on test sett')
-    """
+def check_accuracy(model: nn.Module, data_loader: DataLoader, writer_all, DEVICE, epoch: int, eval_mode: bool = False):
     num_correct = 0
     total_pos, total_neg = 0, 0
     true_pos, true_neg = 0, 0
     true_labels, scores = np.zeros(len(data_loader)), np.zeros(len(data_loader))
 
     # TODO: eval mode changes the mode of dropout and batchnorm layers. Activate this line only after the model is fully learned
-    model.eval()
+    if eval_mode:
+        model.eval()
+    else:
+        model.train()
+
     with torch.no_grad():
         for idx, (data, target, time_list) in enumerate(data_loader):
             if target == 1:
@@ -297,7 +287,10 @@ def check_accuracy(model: nn.Module, data_loader: DataLoader, writer_all, DEVICE
         if data_loader.dataset.train:
             writer_string = 'Train_2'
         else:
-            writer_string = 'Test'
+            if eval_mode:
+                writer_string = 'Test (eval mode)'
+            else:
+                writer_string = 'Test (train mode)'
 
         """
         writer_test.add_scalar('Accuracy', acc, epoch)
@@ -309,12 +302,12 @@ def check_accuracy(model: nn.Module, data_loader: DataLoader, writer_all, DEVICE
         writer_all.add_scalar(writer_string + '/Balanced Accuracy', balanced_acc, epoch)
         writer_all.add_scalar(writer_string + '/Roc-Auc', roc_auc, epoch)
 
-        print('Accuracy of {:.2f}% ({} / {}) over Test set'.format(acc, num_correct, len(data_loader)))
+        print('{}: Accuracy of {:.2f}% ({} / {}) over Test set'.format('EVAL mode' if eval_mode else 'TRAIN mode', acc, num_correct, len(data_loader)))
     model.train()
     return acc, balanced_acc
 
 
-def infer(model: nn.Module, dloader: DataLoader, DEVICE):
+def infer(model: nn.Module, dloader: DataLoader, DEVICE, eval_mode: bool = False):
     """
     This function does inference of a slide from ALL the tiles of the slide
     :param model: model to be used for inference
@@ -325,7 +318,11 @@ def infer(model: nn.Module, dloader: DataLoader, DEVICE):
 
     print('Starting Inference...')
 
-    model.eval()
+    if eval_mode:
+        model.eval()
+    else:
+        model.train()
+
     model.infer = True
     torch.no_grad()
 
@@ -338,9 +335,11 @@ def infer(model: nn.Module, dloader: DataLoader, DEVICE):
     for batch_idx, (data, target) in enumerate(tqdm(dloader)):
         data, target = data.to(DEVICE), target.to(DEVICE)
         model.to(DEVICE)
-        model.get_features = True
+        model.part_1 = True
 
-        features = model(data)
+        features, weights = model(data)
+
+
         """
         if target == 1:
             total_pos += 1
@@ -355,7 +354,7 @@ def infer(model: nn.Module, dloader: DataLoader, DEVICE):
         
         scores_train[batch_idx] = prob.cpu().detach().numpy()[0][0]
         """
-    torch.enable_grad
+    torch.enable_grad()
 
 
 ##################################################################################################

utils.py
@@ -860,8 +860,7 @@ class WSI_MILdataset(Dataset):
             # TODO: transforms.RandomRotation([self.rotate_by, self.rotate_by]),
 
             self.transform = \
-                transforms.Compose([ transforms.RandomVerticalFlip(),
-                                     transforms.RandomHorizontalFlip(),
+                transforms.Compose([ transforms.RandomHorizontalFlip(),
                                      transforms.ToTensor(),
                                      transforms.Normalize(mean=(mean[self.DataSet][0], mean[self.DataSet][1], mean[self.DataSet][2]),
                                                           std=(std[self.DataSet][0], std[self.DataSet][1], std[self.DataSet][2]))
@@ -1148,33 +1147,43 @@ def _save_tile_list_to_file(slide_name: str, tile_list: list, path: str = 'tcga-
 """
 
 
+
 class Infer_WSI_MILdataset(Dataset):
     def __init__(self,
-                 data_path: str = 'tcga-data',
+                 DataSet: str = 'HEROHE',
                  tile_size: int = 256,
-                 tiles_per_bag: int = 50,
+                 bag_size: int = 50,
                  target_kind: str = 'ER',
                  test_fold: int = 1,
-                 train: bool = False,
-                 print_timing: bool = True,
-                 transform=False):
+                 train: bool = True,
+                 print_timing: bool = False,
+                 transform: bool = False,
+                 DX: bool = False):
 
         if target_kind not in ['ER', 'PR', 'Her2']:
             raise ValueError('target should be one of: ER, PR, Her2')
+        if DataSet is 'HEROHE':
+            target_kind = 'Her2'
 
-        meta_data_file = os.path.join(data_path, 'slides_data.xlsx')
+        self.ROOT_PATH = 'All Data'
+        meta_data_file = os.path.join(self.ROOT_PATH, 'slides_data.xlsx')
+        self.DataSet = DataSet
         self.BASIC_MAGNIFICATION = 20
         self.meta_data_DF = pd.read_excel(meta_data_file)
+        if self.DataSet is not 'ALL':
+            self.meta_data_DF = self.meta_data_DF[self.meta_data_DF['id'] == self.DataSet]
+            self.meta_data_DF.reset_index(inplace=True)
 
         # self.meta_data_DF.set_index('id')
-        self.data_path = data_path
+        ### self.data_path = os.path.join(self.ROOT_PATH, self.DataSet)
         self.tile_size = tile_size
         self.target_kind = target_kind
         self.test_fold = test_fold
-        self.tiles_per_bag = tiles_per_bag
+        self.bag_size = bag_size
         self.train = train
         self.print_time = print_timing
         self.transform = transform
+        self.DX = DX
 
         all_targets = list(self.meta_data_DF[self.target_kind + ' status'])
 
@@ -1183,8 +1192,7 @@ class Infer_WSI_MILdataset(Dataset):
         valid_slide_indices = np.where(np.isin(np.array(all_targets), ['Positive', 'Negative']) == True)[0]
 
         # Also remove slides without grid data:
-        slides_without_grid = set(self.meta_data_DF.index[self.meta_data_DF['Total tiles - ' + str(
-            self.tile_size) + ' compatible @ X20'] == -1])
+        slides_without_grid = set(self.meta_data_DF.index[self.meta_data_DF['Total tiles - ' + str(self.tile_size) + ' compatible @ X20'] == -1])
         valid_slide_indices = np.array(list(set(valid_slide_indices) - slides_without_grid))
 
         # BUT...we want the train set to be a combination of all sets except the train set....Let's compute it:
@@ -1201,6 +1209,10 @@ class Infer_WSI_MILdataset(Dataset):
         all_image_path_names = list(self.meta_data_DF['id'])
         all_in_fold = list(self.meta_data_DF['test fold idx'])
         all_tissue_tiles = list(self.meta_data_DF['Legitimate tiles - ' + str(self.tile_size) + ' compatible @ X20'])
+        if self.DataSet is not 'TCGA':
+            self.DX = False
+        if self.DX:
+            all_is_DX_cut = list(self.meta_data_DF['DX'])
 
         all_magnifications = list(self.meta_data_DF['Objective Power'])
 
@@ -1210,73 +1222,128 @@ class Infer_WSI_MILdataset(Dataset):
         self.tissue_tiles = []
         self.target = []
         self.magnification = []
-        self.bags_per_slide = []
-        self.grid_list = []
 
-        for idx, index in enumerate(valid_slide_indices):
-            self.image_file_names.append(all_image_file_names[index])
-            self.image_path_names.append(all_image_path_names[index])
-            self.in_fold.append(all_in_fold[index])
-            self.tissue_tiles.append(all_tissue_tiles[index])
-            self.target.append(all_targets[index])
-            self.magnification.append(all_magnifications[index])
-            self.bags_per_slide.append( -(-all_tissue_tiles[index] // self.tiles_per_bag) )  # round up
+        for _, index in enumerate(valid_slide_indices):
+            if (self.DX and all_is_DX_cut[index]) or not self.DX:
+                self.image_file_names.append(all_image_file_names[index])
+                self.image_path_names.append(all_image_path_names[index])
+                self.in_fold.append(all_in_fold[index])
+                self.tissue_tiles.append(all_tissue_tiles[index])
+                self.target.append(all_targets[index])
+                self.magnification.append(all_magnifications[index])
 
-            grid = _get_grid_list(file_name=os.path.join(self.data_path,
-                                                         self.image_path_names[idx],
-                                                         self.image_file_names[idx]),
-                                  magnification=all_magnifications[index],
-                                  tile_size=self.tile_size)
-            bag = []
-            for i in range(self.bags_per_slide[idx]):
-                if bag == self.bags_per_slide[idx] - 1:
-                    bag.append(grid[(i + 1) * 50:])
-                else:
-                    bag.append(grid[i * 50 : (i + 1) * 50])
+                # The following code lines
 
-            self.grid_list.append(bag)
+
 
         # Setting the transformation:
+        mean = {}
+        std = {}
+
+        mean['TCGA'] = [58.2069073 / 255, 96.22645279 / 255, 70.26442606 / 255]
+        std['TCGA'] = [40.40400300279664 / 255, 58.90625962739444 / 255, 45.09334057330417 / 255]
+
+        mean['HEROHE'] = [31.51838872 / 255, 65.25528123 / 255, 37.49780932 / 255]
+        std['HEROHE'] = [np.sqrt(1110.44122153) / 255, np.sqrt(2950.70309166) / 255, np.sqrt(1027.60832337) / 255]
+
         self.transform = transforms.Compose([transforms.ToTensor(),
-                                             transforms.Normalize(mean=(0.22826, 0.37736, 0.275547),
-                                                                  std=(0.158447, 0.231005, 0.1768365))
+                                             transforms.Normalize(mean=(
+                                                 mean[self.DataSet][0], mean[self.DataSet][1], mean[self.DataSet][2]),
+                                                 std=(std[self.DataSet][0], std[self.DataSet][1], std[self.DataSet][2]))
                                              ])
 
-        self.last_bag = False
-
-        print('Initiation of Inference WSI {} DataSet is Complete. {} Slides, Tiles of size {}^2. {} tiles in a bag, {} Transform'
-              .format('Train' if self.train else 'Test',
-                      len(self.target),
-                      self.tile_size,
-                      self.tiles_per_bag,
-                      'Without' if transform is False else 'With'))
+        print(
+            'Initiation of WSI INFERENCE {} DataSet is Complete. {} Slides, Tiles of size {}^2. {} tiles in a bag, {} Transform. TestSet is fold #{}. DX is {}'
+            .format(self.DataSet,
+                    self.__len__(),
+                    self.tile_size,
+                    self.bag_size,
+                    'Without' if transform is False else 'With',
+                    self.test_fold,
+                    'ON' if self.DX else 'OFF'))
 
     def __len__(self):
-        return sum(self.bags_per_slide)
+        return len(self.target)
 
     def __getitem__(self, idx):
-        start = time.time()
-        # According to idx we need to find which slide and which bag we're in.
+        start_getitem = time.time()
+        data_path_extended = os.path.join(self.ROOT_PATH, self.image_path_names[idx])
+        # file_name = os.path.join(self.data_path, self.image_path_names[idx], self.image_file_names[idx])
+        # tiles = _choose_data(file_name, self.num_of_tiles_from_slide, self.magnification[idx], self.tile_size, print_timing=self.print_time)
+        # tiles, time_list = _choose_data_2(self.data_path, file_name, self.bag_size, self.magnification[idx], self.tile_size, print_timing=self.print_time)
+        '''
+        tiles, time_list = _choose_data_2(data_path_extended, self.image_file_names[idx], self.bag_size, self.magnification[idx],
+                                          self.tile_size, print_timing=self.print_time)
+        '''
+        basic_file_name = '.'.join(self.image_file_names[idx].split('.')[:-1])
+        grid_file = os.path.join(self.ROOT_PATH, self.image_path_names[idx], 'Grids',
+                                 basic_file_name + '--tlsz' + str(self.tile_size) + '.data')
+        image_file = os.path.join(self.ROOT_PATH, self.image_path_names[idx], self.image_file_names[idx])
 
-        grid_locations = self.grid_list
-
-        file_name = os.path.join(self.data_path, self.image_path_names[idx], self.image_file_names[idx])
-        tiles = _choose_data(file_name, self.tiles_per_bag, self.magnification[idx], self.tile_size)
-
+        tiles, time_list = _choose_data_2(grid_file, image_file, self.bag_size,
+                                          self.magnification[idx],
+                                          self.tile_size, print_timing=self.print_time)
         label = [1] if self.target[idx] == 'Positive' else [0]
         label = torch.LongTensor(label)
 
+        """    
+        # This is for images in an nd.array:        
+        shape = tiles.shape
+        X = torch.zeros(shape)
+        """
+
         # The following section is written for tiles in PIL format
-        X = torch.zeros([self.tiles_per_bag, 3, self.tile_size, self.tile_size])
+        X = torch.zeros([self.bag_size, 3, self.tile_size, self.tile_size])
+
+        # Updating RandomRotation angle in the data transformations only for train set:
+        if self.train:
+            rotate_by = sample([0, 90, 180, 270], 1)[0]
+            transform = transforms.Compose([transforms.RandomRotation([rotate_by, rotate_by]),
+                                            self.transform
+                                            ])
+        else:
+            transform = self.transform
+
+        """
+            self.transform = transforms.Compose([transforms.ToTensor(),
+                                                 transforms.Normalize(mean=(0.22826, 0.37736, 0.275547),
+                                                                      std=(0.158447, 0.231005, 0.1768365))
+                                                 ])
+        else:
+            # TODO: Consider using - torchvision.transforms.ColorJitter(brightness=0, contrast=0, saturation=0, hue=0)
+            # TODO: Consider using - torchvision.transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0, inplace=False)
+            # TODO: Consider transforms.RandomHorizontalFlip()
+
+
+            rotate_by = sample([0, 90, 180, 270], 1)[0]
+            self.transform = \
+                transforms.Compose([ transforms.RandomRotation([rotate_by, rotate_by]),
+                                     transforms.RandomVerticalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize(mean=(58.2069073 / 255, 96.22645279 / 255, 70.26442606 / 255),
+                                                          std=(40.40400300279664 / 255, 58.90625962739444 / 255, 45.09334057330417 / 255))
+                                     ])
+        """
+
+        # tiles = tiles.transpose(0, 2, 3, 1)  # When working with PIL, this line is not needed
+        # Check the need to resize the images (in case of different magnification):
 
         magnification_relation = self.magnification[idx] // self.BASIC_MAGNIFICATION
         if magnification_relation != 1:
-            transform = transforms.Compose([transforms.Resize(self.tile_size), self.transform])
+            transform = transforms.Compose([transforms.Resize(self.tile_size), transform])
 
-        for i in range(self.tiles_per_bag):
+        start_aug = time.time()
+        for i in range(self.bag_size):
+            #  X[i] = self.transform(tiles[i])  # This line is for nd.array
             X[i] = transform(tiles[i])
 
+        aug_time = (time.time() - start_aug) / self.bag_size
+        total_time = time.time() - start_getitem
         if self.print_time:
-            end = time.time()
-            print('Infer WSI: Time to prepare item is {:.2f} s'.format(end - start))
-        return X, label, self.last_bag
+            time_list = (time_list[0], time_list[1], aug_time, total_time)
+            # print('Data Augmentation time is: {:.2f} s'.format(aug_time))
+            # print('WSI: TOTAL Time to prepare item is: {:.2f} s'.format(total_time))
+        else:
+            time_list = [0]
+
+        return X, label, time_list