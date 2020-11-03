Bug fixed: - AUC computation for train dataset during training
Added functionality:
- added file inference.py which uses more than 50 tiles per slide in order to infer the class

utils.py
@@ -17,6 +17,10 @@ from typing import List, Tuple
 import shutil
 import cv2 as cv
 
+def chunks(list: List, length: int):
+    new_list = [ list[i * length:(i + 1) * length] for i in range((len(list) + length - 1) // length )]
+    return new_list
+
 def make_dir(dirname):
     if not dirname in next(os.walk(os.getcwd()))[1]:
         try:
@@ -579,6 +583,33 @@ def _get_tiles_2(file_name: str, locations: List[Tuple], tile_sz: int, print_tim
         time_list = [0]
 
     return tiles_PIL, time_list
+
+
+def _get_tile(file_name: str, locations: Tuple, tile_sz: int, print_timing: bool = False):
+    """
+    This function returns an array of tiles
+    :param file_name:
+    :param locations:
+    :param tile_sz:
+    :return:
+    """
+
+    # open the .svs file:
+    start_openslide = time.time()
+    img = openslide.open_slide(file_name)
+    end_openslide = time.time()
+
+    start_gettiles = time.time()
+    image = img.read_region((locations[1], locations[0]), 0, (tile_sz, tile_sz)).convert('RGB')
+    end_gettiles = time.time()
+
+
+    if print_timing:
+        time_list = [end_openslide - start_openslide, end_gettiles - start_gettiles]
+    else:
+        time_list = [0]
+
+    return image, time_list
 
 
 def _get_grid_list(file_name: str, magnification: int = 20, tile_size: int = 256):
@@ -1152,13 +1183,14 @@ class Infer_WSI_MILdataset(Dataset):
     def __init__(self,
                  DataSet: str = 'HEROHE',
                  tile_size: int = 256,
-                 bag_size: int = 50,
+                 tiles_per_iter: int = 3,
                  target_kind: str = 'ER',
-                 test_fold: int = 1,
-                 train: bool = True,
+                 folds: List = [1],
+                 ### train: bool = True,
                  print_timing: bool = False,
-                 transform: bool = False,
-                 DX: bool = False):
+                 ### transform: bool = False,
+                 DX: bool = False,
+                 num_tiles: int = 10):
 
         if target_kind not in ['ER', 'PR', 'Her2']:
             raise ValueError('target should be one of: ER, PR, Her2')
@@ -1174,16 +1206,16 @@ class Infer_WSI_MILdataset(Dataset):
             self.meta_data_DF = self.meta_data_DF[self.meta_data_DF['id'] == self.DataSet]
             self.meta_data_DF.reset_index(inplace=True)
 
-        # self.meta_data_DF.set_index('id')
-        ### self.data_path = os.path.join(self.ROOT_PATH, self.DataSet)
+        self.tiles_per_iter = tiles_per_iter
         self.tile_size = tile_size
         self.target_kind = target_kind
-        self.test_fold = test_fold
-        self.bag_size = bag_size
-        self.train = train
+        self.folds = folds
         self.print_time = print_timing
-        self.transform = transform
         self.DX = DX
+        # The following attributes will be used in the __getitem__ function
+        self.tiles_to_go = None
+        self.slide_num = 0
+        self.current_file = None
 
         all_targets = list(self.meta_data_DF[self.target_kind + ' status'])
 
@@ -1194,13 +1226,6 @@ class Infer_WSI_MILdataset(Dataset):
         # Also remove slides without grid data:
         slides_without_grid = set(self.meta_data_DF.index[self.meta_data_DF['Total tiles - ' + str(self.tile_size) + ' compatible @ X20'] == -1])
         valid_slide_indices = np.array(list(set(valid_slide_indices) - slides_without_grid))
-
-        # BUT...we want the train set to be a combination of all sets except the train set....Let's compute it:
-        if self.train:
-            folds = list(range(1, 7))
-            folds.remove(self.test_fold)
-        else:
-            folds = [self.test_fold]
 
         correct_folds = self.meta_data_DF['test fold idx'][valid_slide_indices].isin(folds)
         valid_slide_indices = np.array(correct_folds.index[correct_folds])
@@ -1222,19 +1247,40 @@ class Infer_WSI_MILdataset(Dataset):
         self.tissue_tiles = []
         self.target = []
         self.magnification = []
+        self.num_patches = []
 
-        for _, index in enumerate(valid_slide_indices):
-            if (self.DX and all_is_DX_cut[index]) or not self.DX:
-                self.image_file_names.append(all_image_file_names[index])
-                self.image_path_names.append(all_image_path_names[index])
-                self.in_fold.append(all_in_fold[index])
-                self.tissue_tiles.append(all_tissue_tiles[index])
-                self.target.append(all_targets[index])
-                self.magnification.append(all_magnifications[index])
-
-                # The following code lines
+        self.image_full_filenames = []
+        self.slide_grids = []
 
 
+        for _, slide_num in enumerate(valid_slide_indices):
+            if (self.DX and all_is_DX_cut[slide_num]) or not self.DX:
+                if num_tiles <= all_tissue_tiles[slide_num]:
+                    self.num_patches.append(num_tiles)
+                else:
+                    self.num_patches.append(all_tissue_tiles[slide_num])
+                    print('Slide available patches are less than {}'.format(num_tiles))
+
+                self.image_file_names.append(all_image_file_names[slide_num])
+                self.image_path_names.append(all_image_path_names[slide_num])
+                self.in_fold.append(all_in_fold[slide_num])
+                self.tissue_tiles.append(all_tissue_tiles[slide_num])
+                self.target.extend([all_targets[slide_num]] * self.num_patches[-1])
+                self.magnification.extend([all_magnifications[slide_num]] * self.num_patches[-1])
+
+                full_image_filename = os.path.join(self.ROOT_PATH, self.image_path_names[-1], self.image_file_names[-1])
+                self.image_full_filenames.append(full_image_filename)
+                basic_file_name = '.'.join(self.image_file_names[-1].split('.')[:-1])
+                grid_file = os.path.join(self.ROOT_PATH, self.image_path_names[-1], 'Grids',
+                                         basic_file_name + '--tlsz' + str(self.tile_size) + '.data')
+                which_patches = sample(range(int(self.tissue_tiles[-1])), self.num_patches[-1])
+
+                with open(grid_file, 'rb') as filehandle:
+                    grid_list = pickle.load(filehandle)
+                chosen_locations = [ grid_list[loc] for loc in which_patches ]
+                chosen_locations_chunks = chunks(chosen_locations, self.tiles_per_iter)
+                self.slide_grids.extend(chosen_locations_chunks)
+                ### self.slide_multiple_filenames.extend([full_image_filename] * self.num_patches[-1])
 
         # Setting the transformation:
         mean = {}
@@ -1253,97 +1299,57 @@ class Infer_WSI_MILdataset(Dataset):
                                              ])
 
         print(
-            'Initiation of WSI INFERENCE {} DataSet is Complete. {} Slides, Tiles of size {}^2. {} tiles in a bag, {} Transform. TestSet is fold #{}. DX is {}'
-            .format(self.DataSet,
-                    self.__len__(),
-                    self.tile_size,
-                    self.bag_size,
-                    'Without' if transform is False else 'With',
-                    self.test_fold,
-                    'ON' if self.DX else 'OFF'))
+            'Initiation of WSI INFERENCE {} DataSet is Complete. {} Slides, Tiles of size {}^2.'
+            .format(self.DataSet, self.__len__(), self.tile_size))
 
     def __len__(self):
-        return len(self.target)
+        ### return len(self.slide_multiple_filenames)
+        return int(np.ceil(np.array(self.num_patches)/3).sum())
 
     def __getitem__(self, idx):
         start_getitem = time.time()
-        data_path_extended = os.path.join(self.ROOT_PATH, self.image_path_names[idx])
-        # file_name = os.path.join(self.data_path, self.image_path_names[idx], self.image_file_names[idx])
-        # tiles = _choose_data(file_name, self.num_of_tiles_from_slide, self.magnification[idx], self.tile_size, print_timing=self.print_time)
-        # tiles, time_list = _choose_data_2(self.data_path, file_name, self.bag_size, self.magnification[idx], self.tile_size, print_timing=self.print_time)
-        '''
-        tiles, time_list = _choose_data_2(data_path_extended, self.image_file_names[idx], self.bag_size, self.magnification[idx],
-                                          self.tile_size, print_timing=self.print_time)
-        '''
-        basic_file_name = '.'.join(self.image_file_names[idx].split('.')[:-1])
-        grid_file = os.path.join(self.ROOT_PATH, self.image_path_names[idx], 'Grids',
-                                 basic_file_name + '--tlsz' + str(self.tile_size) + '.data')
-        image_file = os.path.join(self.ROOT_PATH, self.image_path_names[idx], self.image_file_names[idx])
+        if self.tiles_to_go is None:
+            self.tiles_to_go = self.num_patches[self.slide_num]
+            self.current_file = self.image_full_filenames[self.slide_num]
+            self.initial_num_patches = self.num_patches[self.slide_num]
 
-        tiles, time_list = _choose_data_2(grid_file, image_file, self.bag_size,
-                                          self.magnification[idx],
-                                          self.tile_size, print_timing=self.print_time)
+        if self.tiles_to_go < self.tiles_per_iter:
+            tiles_next_iter = self.tiles_to_go
+            self.tiles_to_go = None
+            self.slide_num += 1
+        else:
+            tiles_next_iter = self.tiles_per_iter
+            self.tiles_to_go -= self.tiles_per_iter
+
+        adjusted_tile_size = self.tile_size * (self.magnification[idx] // self.BASIC_MAGNIFICATION)
+        tiles, time_list = _get_tiles_2(self.current_file,
+                                       self.slide_grids[idx],
+                                       adjusted_tile_size,
+                                       self.print_time)
+
         label = [1] if self.target[idx] == 'Positive' else [0]
         label = torch.LongTensor(label)
 
-        """    
-        # This is for images in an nd.array:        
-        shape = tiles.shape
-        X = torch.zeros(shape)
-        """
-
-        # The following section is written for tiles in PIL format
-        X = torch.zeros([self.bag_size, 3, self.tile_size, self.tile_size])
-
-        # Updating RandomRotation angle in the data transformations only for train set:
-        if self.train:
-            rotate_by = sample([0, 90, 180, 270], 1)[0]
-            transform = transforms.Compose([transforms.RandomRotation([rotate_by, rotate_by]),
-                                            self.transform
-                                            ])
-        else:
-            transform = self.transform
-
-        """
-            self.transform = transforms.Compose([transforms.ToTensor(),
-                                                 transforms.Normalize(mean=(0.22826, 0.37736, 0.275547),
-                                                                      std=(0.158447, 0.231005, 0.1768365))
-                                                 ])
-        else:
-            # TODO: Consider using - torchvision.transforms.ColorJitter(brightness=0, contrast=0, saturation=0, hue=0)
-            # TODO: Consider using - torchvision.transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0, inplace=False)
-            # TODO: Consider transforms.RandomHorizontalFlip()
-
-
-            rotate_by = sample([0, 90, 180, 270], 1)[0]
-            self.transform = \
-                transforms.Compose([ transforms.RandomRotation([rotate_by, rotate_by]),
-                                     transforms.RandomVerticalFlip(),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize(mean=(58.2069073 / 255, 96.22645279 / 255, 70.26442606 / 255),
-                                                          std=(40.40400300279664 / 255, 58.90625962739444 / 255, 45.09334057330417 / 255))
-                                     ])
-        """
-
-        # tiles = tiles.transpose(0, 2, 3, 1)  # When working with PIL, this line is not needed
-        # Check the need to resize the images (in case of different magnification):
+        X = torch.zeros([len(tiles), 3, self.tile_size, self.tile_size])
 
         magnification_relation = self.magnification[idx] // self.BASIC_MAGNIFICATION
         if magnification_relation != 1:
-            transform = transforms.Compose([transforms.Resize(self.tile_size), transform])
+            transform = transforms.Compose([transforms.Resize(self.tile_size), self.transform])
+        else:
+            transform = self.transform
 
         start_aug = time.time()
-        for i in range(self.bag_size):
-            #  X[i] = self.transform(tiles[i])  # This line is for nd.array
+        for i in range(len(tiles)):
             X[i] = transform(tiles[i])
 
-        aug_time = (time.time() - start_aug) / self.bag_size
+        aug_time = time.time() - start_aug
         total_time = time.time() - start_getitem
         if self.print_time:
             time_list = (time_list[0], time_list[1], aug_time, total_time)
-            # print('Data Augmentation time is: {:.2f} s'.format(aug_time))
-            # print('WSI: TOTAL Time to prepare item is: {:.2f} s'.format(total_time))
         else:
             time_list = [0]
-
-        return X, label, time_list
+        if self.tiles_to_go is None:
+            last_batch = True
+        else:
+            last_batch = False
+        return X, label, time_list, last_batch, self.initial_num_patches