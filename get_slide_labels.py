6e64ab7a29b577f8881afaba8c1c9ff97bb7f7f2
RanS <ran.schley@gmail.com>
2021-07-05 12:23:13 +0300

    1. updated get_slide_labels.py to the 4/7/21 annotations file

--- get_slide_labels.py
+++ get_slide_labels.py
@@ -5,66 +5,60 @@ import argparse
 import pandas as pd
 
 parser = argparse.ArgumentParser(description='Slide inspector')
-#parser.add_argument('--in_dir', default=r'\\gipnetappa\public\sgils\BCF scans\Carmel Slides\Batch_1', type=str, help='input dir')
 parser.add_argument('--in_dir', default=r'C:\ran_data\Carmel_Slides_examples\folds_labels', type=str, help='input dir')
 args = parser.parse_args()
 
 in_dir = args.in_dir
+batches = np.arange(1, 9, 1)
 
-meta_data_file = os.path.join(in_dir, 'slides_data.xlsx')
-meta_data_DF = pd.read_excel(meta_data_file)
+for batch in batches:
 
-labels_data_file = os.path.join(os.path.dirname(in_dir), 'Carmel_annotations_6_1_2021.xlsx')
-label_data_DF = pd.read_excel(labels_data_file)
-data_field = ['PR status', 'ER status', 'Her2 status', 'TissueType', 'PatientIndex']
+    meta_data_file = os.path.join(in_dir, 'slides_data_Carmel' + str(batch) + '.xlsx')
+    meta_data_DF = pd.read_excel(meta_data_file)
 
-for ind, slide in enumerate(tqdm(meta_data_DF['patient barcode'])):
-    slide_tissue_id = slide.split('_')[0] + '/' + slide.split('_')[1]
-    slide_label_data = label_data_DF.loc[label_data_DF['TissueID'] == slide_tissue_id]
+    labels_data_file = os.path.join(in_dir, 'Carmel_annotations_04-07-2021.xlsx')
+    label_data_DF = pd.read_excel(labels_data_file)
+    data_field = ['PR status', 'ER status', 'Her2 status', 'TissueType', 'PatientIndex']
 
-    if len(slide_label_data) == 0:
-        print('could not find tissue_id ' + str(slide_tissue_id) + ' in annotations file!')
-    elif len(slide_label_data) == 1:
-        for field in data_field:
-            meta_data_DF.loc[meta_data_DF['patient barcode'] == slide, field] = slide_label_data[field].values[0]
-
-    elif len(slide_label_data) > 1:
-        #edit slide_label_data block_id into list of blocks
-        ind = 0
-        block_id_lists = [[] for i in range(len(slide_label_data))]
-        for block_id in slide_label_data['BlockID'].values:
-            range_split = block_id.split('-')
-            if len(range_split) > 2:
-                print('wrong format for block ' + block_id + ' in slide ' + slide)
-                continue
-            elif len(range_split) == 2:
-                block_id_lists[ind] = [str(block) for block in range(int(range_split[0]), int(range_split[1])+1)] #handle range, should be str??
-            elif len(range_split) == 1:
-                block_id_lists[ind] = block_id.replace(' ', '').split(',') #handle comma/single value
-            ind += 1
-
-        # Search for matches with the appropriate block
-        found = False
+    #for ind, slide in enumerate(tqdm(meta_data_DF['patient barcode'])):
+    for ind, slide in enumerate(meta_data_DF['patient barcode']):
+        slide_tissue_id = slide.split('_')[0] + '/' + slide.split('_')[1]
         slide_block_id = slide.split('_')[2]
-        for ii, block_id_list in enumerate(block_id_lists):
-            if slide_block_id in block_id_list:
-                found = True
-                break #assuming there is only one, taking the first
+        slide_label_data = label_data_DF.loc[label_data_DF['TissueID'] == slide_tissue_id]
 
-        if found:
-            for field in data_field:
-                meta_data_DF.loc[meta_data_DF['patient barcode'] == slide, field] = slide_label_data[field].values[ii]
-        else:
-            #unknown block, take only consensus labels
-            for field in data_field:
-                if np.all(slide_label_data[field].values == slide_label_data[field].values[0]):
+        if len(slide_label_data) == 0:
+            #no matching tissue id
+            print('1. Batch ' + str(batch) + ': could not find tissue_id ' + str(slide_tissue_id) + ' in annotations file!')
+
+        elif len(slide_label_data) == 1:
+            #one matching tissue id, make sure block id is empty or matching
+            BlockID = slide_label_data['BlockID'].item()
+            if np.isnan(BlockID) or str(BlockID) == slide_block_id:
+                for field in data_field:
                     meta_data_DF.loc[meta_data_DF['patient barcode'] == slide, field] = slide_label_data[field].values[0]
-                else:
-                    print('could not determine ' + field + ' for slide ' + slide)
+            else:
+                print('2. Batch ' + str(batch) + ': one matching tissue_id for ', str(slide_tissue_id), ', could not find matching blockID ' + str(slide_block_id) + ' in annotations file!')
 
-#replace empty cells with "missing data"
-for field in data_field:
-    meta_data_DF[field] = meta_data_DF[field].replace(np.nan, 'Missing Data', regex=True)
+        elif len(slide_label_data) > 1:
+            slide_label_data_block = slide_label_data[slide_label_data['BlockID'] == int(slide_block_id)]
+            if len(slide_label_data_block) == 0:
+                print('3: Batch ' + str(batch) + ': ', str(len(slide_label_data)), ' matching tissue_id for ',
+                      str(slide_tissue_id), ', could not find matching blockID ' + slide_block_id + ' in annotations file!')
+            elif len(slide_label_data_block) > 1:
+                print('4: Batch ' + str(batch) + ': ', str(len(slide_label_data)), ' matching tissue_id for ',
+                      str(slide_tissue_id), ', found more than one matching blockID ' + slide_block_id + ' in annotations file!')
+            else:
+                for field in data_field:
+                    meta_data_DF.loc[meta_data_DF['patient barcode'] == slide, field] = slide_label_data[field].values[0]
 
-meta_data_DF.to_excel(os.path.join(in_dir, 'slides_data_labeled.xlsx'))
+    #replace empty cells with "missing data", and 0,1 with "Positive", "Negative"
+    for field in data_field:
+        meta_data_DF[field] = meta_data_DF[field].replace(np.nan, 'Missing Data', regex=True)
+        meta_data_DF[field] = meta_data_DF[field].replace(1, 'Positive', regex=True)
+        meta_data_DF[field] = meta_data_DF[field].replace(0, 'Negative', regex=True)
+
+
+
+
+    meta_data_DF.to_excel(os.path.join(in_dir, 'slides_data_CARMEL'+ str(batch)+ '_labeled.xlsx'))
 print('finished')
